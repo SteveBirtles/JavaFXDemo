8b41b172b97cdcc94715bb9ab35695492251199e
Steve Birtles <sbirtles@farnborough.ac.uk>
2016-06-21 12:56:03 +0100

    Added code for second scene launch

--- SceneController.java
+++ SceneController.java
@@ -1,4 +1,6 @@
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
