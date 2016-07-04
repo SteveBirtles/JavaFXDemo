c7b68c95b2cf4b2837aacdcd7359bd701e2eb1e2
Steve Birtles <sbirtles@farnborough.ac.uk>
2016-07-04 12:54:09 +0100

    Ready for lesson

--- Application.java
+++ Application.java
@@ -1,21 +1,19 @@
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import javafx.event.EventHandler;
+import javafx.stage.WindowEvent;
+import javafx.event.ActionEvent;
 import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 
-/* This is the first class for our JavaFX application that contains the main method. As well as setting
- * everything in motion, it also contains a method to stop the program running. */
 public class Application
 {
-    /* Our app needs a connection to the database, and this is all handled by the DatabaseConnection 
-     * class. A public static variable, called database, points to the DatabaseConnection object. */
-    public static DatabaseConnection database;
 
     public static void main(String args[])
     {       
-        /* JavaFX apps run in a different processing thread to the main method so that they keep running
-         * after the main method has completed. The 'start' method is invoked when this thread starts. */
         JFXPanel panel = new JFXPanel();        
         Platform.runLater(() -> start());               
     }
@@ -24,37 +22,37 @@ public class Application
     {
         try
         {         
-            database = new DatabaseConnection("Test.db");        // Initiate the database connection.
 
-            /* Load the first fxml file that will create our first JavaFX scene. */
-            FXMLLoader loader = new FXMLLoader(Application.class.getResource("DemoScene.fxml"));
+            Pane rootPane = new Pane();
 
-            /* Each scene requires a Window, called a stage. The following creates and displays the stage. */
             Stage stage = new Stage();
-            stage.setTitle("JavaFX Demo");
-            stage.setScene(new Scene(loader.load()));
-            stage.show();           
+            stage.setTitle("JavaFX Dynamic Scene Demo");
+            stage.setResizable(false);
+            stage.setScene(new Scene(rootPane));                        
+            stage.setWidth(1024);
+            stage.setHeight(768);
+            stage.setOnCloseRequest((WindowEvent we) -> Application.terminate());
+            stage.show(); 
 
-            /* Loading the scene will have resulted in the creation of a scene controller. In order for this
-             * controller to have direct access to the stage its scene is displayed, the stage is passed to the 
-             * controller's prepareStage method. */
-            SceneController controller = loader.getController();
-            controller.prepareStageEvents(stage);
+            Button btn = new Button();
+            btn.setText("Hello");
+            btn.setLayoutX(50);
+            btn.setLayoutY(50);
+            btn.setOnAction((ActionEvent ae) -> System.out.println("Hello World!"));
+
+            rootPane.getChildren().add(btn);
+
         }
-        catch (Exception ex)    // If anything goes wrong starting the application then call the terminate method.
+        catch (Exception ex)
         {
             System.out.println(ex.getMessage());
             terminate();
         }
     }
 
-    /* The following method can be called from any controller class and will terminate the application. */
     public static void terminate()
     {
-        System.out.println("Closing database connection and terminating application...");                                
-
-        if (database != null) database.disconnect();    // Close the connection to the database (if it exists!)
-        System.exit(0);                                 // Finally, terminate the entire application.
+        System.exit(0);
     }
 
 }
